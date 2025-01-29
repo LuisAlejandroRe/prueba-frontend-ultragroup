9764669e070f7fffac33d54efdd6ae12b4ad5e97
Alejandro <realpealejo@gmail.com>
2025-01-28 23:02:41 -0500

    feat: Setup tailwind and prettier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <Link to="/about">About</Link>
-      <h1>Home Page</h1>
+      <h1 className="text-4xl text-blue-500">Home Page</h1>
     </div>
   );
 };
